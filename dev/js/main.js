13d58d9d6e17fec71f3ae0d985c1f6a8d68eeda6
Pastora Isabel Osegueda Morales <posegueda@b7-10.local>
2017-11-24 19:39:42 -0600

    subiendo random

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,108 +1,45 @@
+var cards = new Array(
+    { numberCard: '1', seleccion: false }, { numberCard: '2', seleccion: false },
+    { numberCard: '0', seleccion: false }, { numberCard: '9', seleccion: false },
+    { numberCard: '3', seleccion: false }, { numberCard: '4', seleccion: false },
+    { numberCard: '5', seleccion: false }, { numberCard: '6', seleccion: false },
+    { numberCard: '7', seleccion: false }, { numberCard: '8', seleccion: false },
+    { numberCard: '1', seleccion: false }, { numberCard: '2', seleccion: false },
+    { numberCard: '3', seleccion: false }, { numberCard: '4', seleccion: false },
+    { numberCard: '5', seleccion: false }, { numberCard: '6', seleccion: false },
+    { numberCard: '0', seleccion: false }, { numberCard: '9', seleccion: false },
+    { numberCard: '7', seleccion: false }, { numberCard: '8', seleccion: false });
 
-var cartas = new Array(
-    { nombre: '9', seleccion: false }, { nombre: '10', seleccion: false},
-    { nombre: '1', seleccion: false }, { nombre: '2', seleccion: false },
-    { nombre: '3', seleccion: false }, { nombre: '4', seleccion: false },
-    { nombre: '5', seleccion: false }, { nombre: '6', seleccion: false },
-    { nombre: '7', seleccion: false }, { nombre: '8', seleccion: false },
-    { nombre: '1', seleccion: false }, { nombre: '2', seleccion: false },
-    { nombre: '3', seleccion: false }, { nombre: '4', seleccion: false },
-    { nombre: '5', seleccion: false }, { nombre: '6', seleccion: false },
-    { nombre: '7', seleccion: false }, { nombre: '8', seleccion: false },
-    { nombre: '9', seleccion: false }, { nombre: '10', seleccion: false });
 
+function startGame() {
+    var data = document.getElementById("game");
+        cards.sort(function () { return Math.random() - 0.5 });
+        for (var i = 0; i < 20; i++) {
+            var card = cards[i].numberCard;
+            var data = document.getElementById(i.toString());
+            data.dataset.id = card;
+            data.src = 'img/' + card + '.png';
+            console.log(card);       
+        } 
+        setTimeout(() => {
+            for (var i = 0; i < 20; i++) {
+            var card = cards[i].numberCard;
+            var data = document.getElementById(i.toString());
+            data.dataset.id = card;
+            data.src = 'img/bigTop.png';
+            }                 
+        }, 3000);       
+}
 
-var intentos = 0;
-var jugada1 = "";
-var jugada2 = "";
-var identificadorJ1 = "";
-var identificadorJ2 = "";
-
-function iniciarJuego() {
-    var dato = document.getElementById("juego");
-    alert("empieza el juego");
-
-    cartas.sort(function () { return Math.random() - 0.5 });
+function resetGame() {
+    var data = document.getElementById("game");
+    cards.sort(function () { return Math.random() - 0.5 });
     for (var i = 0; i < 20; i++) {
-        var carta = cartas[i].nombre;
-        var dato = document.getElementById(i.toString());
-        dato.dataset.valor = carta;
-        imageChange(i, "img/bigTop.png", 'holi');
-        
-    }
-};
-
-function resetearJuego() {
-    cartas.sort(function () { return Math.random() - 0.5 });
-    for (var i = 0; i < 20; i++) {
-        var carta = cartas[i].nombre;
-        var dato = document.getElementById(i.toString());
-        dato.dataset.valor = carta;
-        imageChange(i, "img/bigTop.png", 'holi');
-    }
+        var card = cards[i].numberCard;
+        var data = document.getElementById(i.toString());
+        data.dataset.id = card;
+    }    
 }
 
-function girarCarta() {
-    var evento = window.event;
-
-    jugada2 = evento.target.dataset.valor;
-    identificadorJ2 = evento.target.id;
-
-
-    if (jugada1 !== "") {
-
-        if (jugada1 === jugada2 && identificadorJ1 !== identificadorJ2 && cartas[parseInt(identificadorJ2)].seleccion != true && cartas[parseInt(identificadorJ1)].seleccion != true) {
-
-            cartas[parseInt(identificadorJ1)].seleccion = true;
-            cartas[parseInt(identificadorJ2)].seleccion = true;
-
-            imageChange(identificadorJ2, "img/clown.png", jugada2);
-            vaciar();
-            comprobar(); console.log(indentificadorJ2);
-        } else if (identificadorJ1 !== identificadorJ2) {
-            var self = this;
-            setTimeout(function () {
-                imageChange(self.identificadorJ1, "img/bigTop", "?")
-                imageChange(self.identificadorJ2, "img/bigTop", "?")
-                vaciar()
-            }, 200);
-
-            imageChange(identificadorJ2, "img/clown.png" , jugada2);
-        }
-    } else if (jugada2 !== "valor") {
-
-        imageChange(identificadorJ2, "img/clown.png", jugada2);
-
-        jugada1 = jugada2;
-        identificadorJ1 = identificadorJ2;
-    }
-};
-
-function vaciar() {
-    jugada1 = "";
-    jugada2 = "";
-
-    identificadorJ1 = "";
-    identificadorJ2 = "";
-}
-
-function imageChange(position, image, content) {
-    document.getElementById(position.toString()).src = image;
-    document.getElementById(position.toString()).innerHTML = content;
-}
-
-function comprobar() {
-    var aciertos = 0;
-    for (var i = 0; i < 20; i++) {
-        if (cartas[i].seleccion == true) {
-            aciertos++;
-        }
-
-    }
-
-    if (aciertos == 20) {
-        document.getElementById("juego").innerHTML = "GANASTE";
-    }
-}
 
 
