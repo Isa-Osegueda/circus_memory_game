9eb83bfede5be0b159988539725995294e80a061
Pastora Isabel Osegueda Morales <posegueda@b7-10.local>
2017-12-01 17:51:16 -0600

    subiendo avance

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,15 +1,19 @@
 var cards = new Array(
-    { numberCard: '1', seleccion: false }, { numberCard: '2', seleccion: false },
-    { numberCard: '0', seleccion: false }, { numberCard: '9', seleccion: false },
-    { numberCard: '3', seleccion: false }, { numberCard: '4', seleccion: false },
-    { numberCard: '5', seleccion: false }, { numberCard: '6', seleccion: false },
-    { numberCard: '7', seleccion: false }, { numberCard: '8', seleccion: false },
-    { numberCard: '1', seleccion: false }, { numberCard: '2', seleccion: false },
-    { numberCard: '3', seleccion: false }, { numberCard: '4', seleccion: false },
-    { numberCard: '5', seleccion: false }, { numberCard: '6', seleccion: false },
-    { numberCard: '0', seleccion: false }, { numberCard: '9', seleccion: false },
-    { numberCard: '7', seleccion: false }, { numberCard: '8', seleccion: false });
+    { numberCard: '1', selection: false }, { numberCard: '2', selection: false },
+    { numberCard: '0', selection: false }, { numberCard: '9', selection: false },
+    { numberCard: '3', selection: false }, { numberCard: '4', selection: false },
+    { numberCard: '5', selection: false }, { numberCard: '6', selection: false },
+    { numberCard: '7', selection: false }, { numberCard: '8', selection: false },
+    { numberCard: '1', selection: false }, { numberCard: '2', selection: false },
+    { numberCard: '3', selection: false }, { numberCard: '4', selection: false },
+    { numberCard: '5', selection: false }, { numberCard: '6', selection: false },
+    { numberCard: '0', selection: false }, { numberCard: '9', selection: false },
+    { numberCard: '7', selection: false }, { numberCard: '8', selection: false });
 
+var play2 = "";
+var play1 = "";
+var identifyJ1 = "";
+var identifyJ2 ="";
 
 function startGame() {
     var data = document.getElementById("game");
@@ -19,7 +23,7 @@ function startGame() {
             var data = document.getElementById(i.toString());
             data.dataset.id = card;
             data.src = 'img/' + card + '.png';
-            console.log(card);       
+                   
         } 
         setTimeout(() => {
             for (var i = 0; i < 20; i++) {
@@ -38,8 +42,40 @@ function resetGame() {
         var card = cards[i].numberCard;
         var data = document.getElementById(i.toString());
         data.dataset.id = card;
-    }    
+    }   
 }
+
+
+function turnLetter() {
+    var action = window.event;
+
+    play2 = action.target.dataset.id;
+    identifyJ2 = action.target;
+    var selection2 = cards.selection;
+    var selection1 = cards.selection;
+    console.log(identifyJ2);
+    console.log(play2);
+
+    
+      
+    if(play1 == ""){
+
+        var img = identifyJ2.dataset.id;
+        console.log(img);
+        identifyJ2.src = 'img/' + img + '.png';
+
+        if(img === play2){
+            console.log('pareja encontrada');
+        }
+
+    }
+   
+   
+};
+
+
+
+
 
 
 
